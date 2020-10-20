f5eb54ac65514f9b7158f038033314ed22751639
Andrey Karabash <andrey.karabash@orioninc.com>
2020-10-20 12:17:42 +0300

    feat/54044 ww

--- src/main/java/com/orc/ak/App4.java
+++ src/main/java/com/orc/ak/App4.java
@@ -10,6 +10,7 @@ public class App4
 {
     public static void main( String[] args )
     {
+        // feat/54044 ww
         // feat/52030 zzz + 32
         // feat/52002 add.21 add.22
         // feat.50011 jkgjgkjg kjh g
