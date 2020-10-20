1573a328fef0014d10a8211183855040c9771315
Andrey Karabash <andrey.karabash@orioninc.com>
2020-10-20 13:43:10 +0300

    feat/55222 z2

--- src/main/java/com/orc/ak/App4.java
+++ src/main/java/com/orc/ak/App4.java
@@ -10,6 +10,8 @@ public class App4
 {
     public static void main( String[] args )
     {
+        // feat/55222 z2
+
         // feat/54044 ww2
         // feat/54004 c1+2
         // feat/52030 zzz + 32
