5c466a49021d825972df34856a5c9382b5482a28
Andrey Karabash <andrey.karabash@orioninc.com>
2020-10-20 15:13:46 +0300

    feat/81000 add

--- src/main/java/com/orc/ak/App4.java
+++ src/main/java/com/orc/ak/App4.java
@@ -11,7 +11,7 @@ public class App4
     public static void main( String[] args )
     {
         //-=-=-=-[ Header ]-=-=-=
-        // feat/81000
+        // feat/81000 addition
         // feat/72000 zzz
         // feat/71000 a
         // feat/55222 upd
