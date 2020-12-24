cd77e1fabbfc6fe08953913b411ae39805488390
Andrey Karabash <andrey.karabash@orioninc.com>
2020-12-24 16:55:15 +0300

    feat/93003 start

--- src/main/java/com/orc/ak/App4.java
+++ src/main/java/com/orc/ak/App4.java
@@ -11,6 +11,7 @@ public class App4
     public static void main( String[] args )
     {
         //-=-=-=-[ HEADER ]-=-=-=
+        // feat/93003 start
         // feat/91000 start
         // feat/90002 st-chg1-chg2
         // feat/82000 upd + fix
