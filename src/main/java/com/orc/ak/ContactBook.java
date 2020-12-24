d0567508ec644817b72bd94cec9c39d76546326e
Andrey Karabash <andrey.karabash@orioninc.com>
2020-12-24 17:04:05 +0300

    feat/93001 change22

--- src/main/java/com/orc/ak/ContactBook.java
+++ src/main/java/com/orc/ak/ContactBook.java
@@ -12,7 +12,7 @@ import java.util.stream.Stream;
 public class ContactBook {
 
     // feat/91000 change2
-    // feat/93001 change21
+    // feat/93001 change22
 
     protected List<ContactCard> contactCards;
 
